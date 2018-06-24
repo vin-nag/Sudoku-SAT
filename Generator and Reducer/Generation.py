ab7b0bf9605ce1afa82482d9e3051b3619959c6d
Vineel Nagisetty <vineel.nagisetty@gmail.com>
2018-06-24 20:22:10 -0230

    Added a Generation, Reduction file and a Sudoku Solver (README) notebook file, as well as slightly modified the Sudoku.py to print out generated values as a list

---
+++ Generator and Reducer/Generation.py
@@ -0,0 +1,72 @@
+from sudoku import Sudoku
+import pickle
+
+class Generator:
+    """
+    A class that generates random instances of our sudoku board problem.
+    It takes in a size, k_value and path and generates a random sudoku board based on this.
+    """
+
+    def __init__(self, path='output', size=9, k_value=13):
+        """
+        Standard Initializer
+        :param path: filename path to store the generated board
+        :param size: size of the Sudoku board
+        :param k_value: The number of random values to generate
+        """
+        self.size = size
+        self.k_value = k_value
+        self.board = Sudoku(self.size)
+        self.path = path
+        self.d = dict()
+        return
+
+    def generate(self):
+        """
+        This method generates a random Sudoku board using a method from Sudoku class
+        :return: None
+        """
+        # Generate random Sudoku board
+        self.board.randomize_board(self.k_value)
+
+        # record the random values generated in a list
+        result = self.board.regular_output()
+
+        # Store the generated values in a dictionary
+        for item in result:
+            item[0]+=1
+            item[1]+=1
+            key = str(item[0]) + str(item[1])
+            value = str(item[2])
+            self.d[key] = value
+
+        print("Generated Board: ")
+        self.print_board()
+
+        # Store the generated values using pickle
+        with open(self.path, 'wb') as fp:
+            pickle.dump(result, fp)
+        return
+
+
+    def print_board(self):
+        """
+        This method prints out the Sudoku board in a readable format
+        :return: None
+        """
+        rows = '123456789'
+        cols = '123456789'
+
+        # this part prints out the board in a human readable format
+        for i,r in enumerate(rows):
+            if i in [3,6]:
+                print('------+-------+------')
+            for j,c in enumerate(cols):
+                if j in [3,6]:
+                    print('|',end=" ")
+                if r+c in self.d:
+                    print (self.d[r + c], end=" ")
+                else:
+                    print(0, end = " ")
+            print(" ")
+        return
--- Generator and Reducer/Reduction.py
+++ Generator and Reducer/Reduction.py
@@ -1,18 +1,210 @@
 import pickle
+import numpy as np
+import itertools as it
 
 class Reducer():
+    """
+    This class take as an input a name of a file containing an instance of our Sudoku problem and
+    produces a file with a CNF formula encoding of this instance in .cnf format.
+    """
 
-    def __init__(self, path='output'):
+    def __init__(self, path='output', size=9, k_value=13):
+        """
+        Standard Initializer
+        :param path: path of the file created by our Generator
+        :param size: size of the Sudoku board
+        :param k_value: number of random values generated
+        """
+        self.size = size
+        self.k_value = k_value
         self.path = path
+        self.cnf = ""
+        return
 
-    def get_cnf_data(self, cnf_path='sudoku9.cnf'):
-        with open(cnf_path) as f:
-            cnf=f.readlines()
-        print(cnf)
 
     def create_cnf_file(self):
+        """
+        This method creates the cnf file
+        :return: None
+        """
+        # Update the cnf string with all the Sudoku constraints in cnf form
+        self.get_Sudoku_cnf_constraints()
+
+        # Update the cnf string with the generated board
+        self.get_generated_data()
+
+        # Update the header value from info generated
+        cnf_header = self.create_cnf_header()
+
+        # print all values to a .cnf format file
+        with open(self.path+'.cnf', 'w') as f:
+            f.write(cnf_header)
+            f.write(self.cnf)
+        return
+
+
+    def get_generated_data(self):
+        """
+        This method gets the generated board data and updates to cnf string
+        :return: None
+        """
+        # Get the values of the generated board
         with open(self.path, 'rb') as fp:
             itemlist = pickle.load(fp)
 
-        print(itemlist)
+        # update the string
+        for item in itemlist:
+            self.cnf += (''.join(map(str, item)) + ' 0\n')
+        return
 
+
+    def create_cnf_header(self):
+        """
+        This method creates a header
+        :return: cnf header as a string
+        """
+        # add info to the comments
+        cnf_comment = 'c Sudoku-SAT cnf file by Bishop, Robert; Graves, Caleb; and Nagisetty, Vineel \n'
+        cnf_comment += 'c Size of board =' + str(self.size) + " by " + str(self.size) + ' \n'
+        cnf_comment += 'c k Value = ' + str(self.k_value) + ' \n'
+
+        # find number of clauses
+        clause_count = str.count(self.cnf, "0")
+
+        var_count = str(self.size) + str(self.size) + str(self.size)
+        # add variable number, clause number to problem line
+        cnf_p_line = 'p cnf ' + var_count + ' ' + str(clause_count) + '\n'
+        return cnf_comment + cnf_p_line
+
+
+    def get_Sudoku_cnf_constraints(self):
+        """
+        This method updates the cnf string with all of the Sudoku game constraints
+        :return: None
+        """
+        self.cnf += self.cell_definedness()
+        self.cnf += self.cell_uniqueness()
+        self.cnf += self.row_definedness()
+        self.cnf += self.row_uniqueness()
+        self.cnf += self.column_definedness()
+        self.cnf += self.column_uniqueness()
+        self.cnf += self.block_definedness()
+        self.cnf += self.block_uniqueness()
+        return
+
+
+    def cell_definedness(self):
+        """
+        This method updates the cnf string with the cell definedness constraint
+        :return: cell definedness constraint as a string
+        """
+        line = ""
+        for row in range(self.size):
+            for column in range(self.size):
+                for value in range(self.size):
+                    line += "{}{}{} ".format(row+1, column+1, value+1)
+                line+= " 0\n"
+        return line
+
+
+    def cell_uniqueness(self):
+        """
+        This method updates the cnf string with the cell uniqueness constraint
+        :return: cell uniqueness constraint as a string
+        """
+        line = ""
+        for row in range(self.size):
+            for column in range(self.size):
+                for vi, vj in it.combinations(range(self.size), 2):
+                    line += "-{}{}{} -{}{}{} 0\n".format(row + 1, column + 1, vi + 1, row + 1, column + 1, vj + 1)
+        return line
+
+
+    def row_definedness(self):
+        """
+        This method updates the cnf string with the row definedness constraint
+        :return: row definedness constraint as a string
+        """
+        line = ""
+        for row in range(self.size):
+            for value in range(self.size):
+                for column in range(self.size):
+                    line += "{}{}{} ".format(row + 1, column + 1, value + 1)
+                line += " 0\n"
+        return line
+
+
+    def row_uniqueness(self):
+        """
+         This method updates the cnf string with the row uniqueness constraint
+        :return: row uniqueness constraint as a string
+        """
+        line = ""
+        for row in range(self.size):
+            for value in range(self.size):
+                for ci, cj in it.combinations(range(self.size), 2):
+                    line += "-{}{}{} -{}{}{} 0\n".format(row + 1, ci + 1, value + 1, row + 1, cj + 1, value + 1)
+        return line
+
+
+    def column_definedness(self):
+        """
+         This method updates the cnf string with the column definedness constraint
+        :return: column definedness constraint as a string
+        """
+        line = ""
+        for column in range(self.size):
+            for value in range(self.size):
+                for row in range(self.size):
+                    line += "{}{}{} ".format(row + 1, column + 1, value + 1)
+                line += " 0\n"
+        return line
+
+
+    def column_uniqueness(self):
+        """
+         This method updates the cnf string with the column uniqueness constraint
+        :return: column uniqueness constraint as a string
+        """
+        line = ""
+        for column in range(self.size):
+            for value in range(self.size):
+                for ri, rj in it.combinations(range(self.size), 2):
+                    line += "-{}{}{} -{}{}{} 0\n".format(ri + 1, column + 1, value + 1, rj + 1, column + 1, value + 1)
+        return line
+
+
+    def block_definedness(self):
+        """
+         This method updates the cnf string with the block definedness constraint
+        :return: block definedness constraint as a string
+        """
+        line = ""
+        rootn = int(np.sqrt(self.size))
+        for roff in range(rootn):
+            for coff in range(rootn):
+                for value in range(self.size):
+                    for row in range(rootn):
+                        for column in range(rootn):
+                            line += "{}{}{} ".format(roff * rootn + row + 1, coff * rootn + column + 1, value + 1)
+                    line += ' 0\n'
+        return line
+
+
+    def block_uniqueness(self):
+        """
+         This method updates the cnf string with the block uniqueness constraint
+        :return: block uniqueness constraint as a string
+        """
+        line=""
+        rootn = int(np.sqrt(self.size))
+        for r in range(rootn):
+            for c in range(rootn):
+                for v in range(self.size):
+                    square = []
+                    for r1 in range(rootn):
+                        for c1 in range(rootn):
+                            square.append("{}{}".format(r * rootn + r1 + 1, c * rootn + c1 + 1))
+                    for s1, s2 in it.combinations(square, 2):
+                        line += "-{}{} -{}{} 0\n".format(s1, v + 1, s2, v + 1)
+        return line
